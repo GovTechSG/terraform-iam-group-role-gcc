55da54bfcb27545c2666382faa6ddefccbcfcb21
Ryan Goh <ryangohjq@gmail.com>
2021-10-22 10:52:50 +0800

    feat: changed inline policy to separate policy to be attached

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "aws_iam_policy_document" "trusted_account" {
   statement {
     sid = "TrustedAccounts"
     actions = [
-      "sts:AssumeRole"]
+    "sts:AssumeRole"]
     principals {
       type = "AWS"
       # root looks scary but this is just a trust policy so that we can attach the actual
@@ -25,7 +25,7 @@ data "aws_iam_policy_document" "trusted_account" {
     }
     # Only allow role to be assumed if MFA token is present
     condition {
-      test = "Bool"
+      test     = "Bool"
       variable = "aws:MultiFactorAuthPresent"
       values = [
         "true",
@@ -43,12 +43,12 @@ data "aws_iam_policy_document" "trusted_roles" {
       "sts:AssumeRole"
     ]
     principals {
-      type = "AWS"
+      type        = "AWS"
       identifiers = var.trusted_root_accounts
     }
     # Only allow role to be assumed if MFA token is present
     condition {
-      test = "StringEquals"
+      test     = "StringEquals"
       variable = "sts:ExternalId"
       values = [
         var.external_id
@@ -77,45 +77,53 @@ data "aws_iam_policy_document" "iam_rolling" {
       "sts:AssumeRole",
     ]
     resources = formatlist("arn:aws:iam::${data.aws_caller_identity.current.account_id}:group/%s", var.group_names)
-    effect = "Allow"
+    effect    = "Allow"
   }
 }
 
 # ref: https://www.terraform.io/docs/providers/aws/r/iam_role.html
 resource "aws_iam_role" "iam_role" {
-  name = var.name
-  path = var.path
-  description = var.description
-  permissions_boundary = var.enable_gcci_boundary ? "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/GCCIAccountBoundary" : ""
-  assume_role_policy = data.aws_iam_policy_document.iam_trusted.json
-  max_session_duration = var.max_session_duration
+  name                  = var.name
+  path                  = var.path
+  description           = var.description
+  permissions_boundary  = var.enable_gcci_boundary ? "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/GCCIAccountBoundary" : ""
+  assume_role_policy    = data.aws_iam_policy_document.iam_trusted.json
+  max_session_duration  = var.max_session_duration
   force_detach_policies = true
 }
 
 # ref: https://www.terraform.io/docs/providers/aws/r/iam_policy.html
 resource "aws_iam_policy" "iam_allowing_group_to_assume_role" {
-  name = "${var.name}-group-enabler"
+  name        = "${var.name}-group-enabler"
   description = "allows for groups [${join(",", var.group_names)}] to assume role/${var.name}"
-  policy = data.aws_iam_policy_document.iam_rolling.json
+  policy      = data.aws_iam_policy_document.iam_rolling.json
 }
 
 # ref: https://www.terraform.io/docs/providers/aws/r/iam_role_policy_attachment.html
 resource "aws_iam_role_policy_attachment" "iam_grouping" {
-  role = aws_iam_role.iam_role.name
+  role       = aws_iam_role.iam_role.name
   policy_arn = aws_iam_policy.iam_allowing_group_to_assume_role.arn
-}
-
-# Only create the custom inline policy for this role if it's not empty
-resource "aws_iam_role_policy" "custom_policy" {
-  name = "custom_policy"
-  role = aws_iam_role.iam_role.name
-  count = var.custom_policy != "" ? 1 : 0
-  policy = var.custom_policy
 }
 
 # Maps the given list of existing policies to the role
 resource "aws_iam_role_policy_attachment" "attach_policy" {
-  role = aws_iam_role.iam_role.name
-  for_each = var.attach_policies
+  role       = aws_iam_role.iam_role.name
+  for_each   = var.attach_policies
   policy_arn = each.value
 }
+
+resource "aws_iam_policy" "policy" {
+  count       = var.custom_policy != "" ? 1 : 0
+  name        = "${var.name}-policy"
+  description = var.description
+
+  policy = var.custom_policy
+}
+
+resource "aws_iam_role_policy_attachment" "attach_custom_policy" {
+  count      = var.custom_policy != "" ? 1 : 0
+  role       = aws_iam_role.iam_role.name
+  policy_arn = aws_iam_policy.policy[0].arn
+}
+
+# Maps the given list of existing policies to the role
--- outputs.tf
+++ outputs.tf
@@ -1,69 +1,85 @@
 output "arn" {
   description = "arn of the role"
-  value       = "${aws_iam_role.iam_role.arn}"
+  value       = aws_iam_role.iam_role.arn
 }
 
 output "create_date" {
   description = "date which the role was created"
-  value       = "${aws_iam_role.iam_role.create_date}"
+  value       = aws_iam_role.iam_role.create_date
 }
 
 output "description" {
   description = "description of the role"
-  value       = "${aws_iam_role.iam_role.description}"
+  value       = aws_iam_role.iam_role.description
 }
 
 output "id" {
   description = "id of the role"
-  value       = "${aws_iam_role.iam_role.id}"
+  value       = aws_iam_role.iam_role.id
 }
 
 output "name" {
   description = "name of the role"
-  value       = "${aws_iam_role.iam_role.name}"
+  value       = aws_iam_role.iam_role.name
 }
 
 output "policy" {
   description = "policy attached to this role"
-  value       = "${aws_iam_policy.iam_allowing_group_to_assume_role.policy}"
+  value       = aws_iam_policy.iam_allowing_group_to_assume_role.policy
 }
 
 output "policy_arn" {
   description = "arn of policy attached to this role"
-  value       = "${aws_iam_policy.iam_allowing_group_to_assume_role.arn}"
+  value       = aws_iam_policy.iam_allowing_group_to_assume_role.arn
 }
 
 output "policy_description" {
   description = "description of policy attached to this role"
-  value       = "${aws_iam_policy.iam_allowing_group_to_assume_role.description}"
+  value       = aws_iam_policy.iam_allowing_group_to_assume_role.description
 }
 
 output "policy_id" {
   description = "id of policy attached to this role"
-  value       = "${aws_iam_policy.iam_allowing_group_to_assume_role.id}"
+  value       = aws_iam_policy.iam_allowing_group_to_assume_role.id
 }
 
 output "policy_name" {
   description = "name of policy attached to this role"
-  value       = "${aws_iam_policy.iam_allowing_group_to_assume_role.name}"
+  value       = aws_iam_policy.iam_allowing_group_to_assume_role.name
 }
 
 output "policy_path" {
   description = "path of policy attached to this role"
-  value       = "${aws_iam_policy.iam_allowing_group_to_assume_role.path}"
+  value       = aws_iam_policy.iam_allowing_group_to_assume_role.path
 }
 
 output "trust_policy" {
   description = "trust role policy of this role"
-  value       = "${data.aws_iam_policy_document.iam_trusted.json}"
+  value       = data.aws_iam_policy_document.iam_trusted.json
 }
 
 output "unique_id" {
   description = "unique id of the role"
-  value       = "${aws_iam_role.iam_role.unique_id}"
+  value       = aws_iam_role.iam_role.unique_id
 }
 
 output "role_session_duration" {
   description = "maximum duration a role can be assume for"
-  value       = "${aws_iam_role.iam_role.max_session_duration}"
+  value       = aws_iam_role.iam_role.max_session_duration
 }
+
+output "custom_policy_arn" {
+  description = "ARN of the custom policy"
+  value       = aws_iam_policy.policy[0].arn
+}
+
+output "custom_policy_id" {
+  description = "id of the custom policy"
+  value       = aws_iam_policy.policy[0].id
+}
+
+output "custom_policy_name" {
+  description = "name of the custom policy"
+  value       = aws_iam_policy.policy[0].name
+}
+
